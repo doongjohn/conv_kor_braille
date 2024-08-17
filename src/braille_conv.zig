21f88ab77b83067c89881471e2e54ca998ed510a
doongjohn <doongjohn@gmail.com>
2024-08-17 16:02:23 +0900

    Refactor code and add some tests

--- src/braille_conv.zig
+++ src/braille_conv.zig
@@ -1,42 +1,106 @@
 const std = @import("std");
 
+const kor_char_lookup = struct {
+    const cho = [_]u21{ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
+    const jung = [_]u21{ 'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ' };
+    const jong = [_]u21{ '?', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
+};
+
+const kor_braille_lookup = struct {
+    const cho = [_]u21{ '⠈', 0, '⠉', '⠊', 1, '⠐', '⠑', '⠘', 2, '⠠', 3, '⠛', '⠨', 4, '⠰', '⠋', '⠓', '⠙', '⠚' };
+    const cho2 = [_][2]u21{
+        .{ '⠠', '⠈' }, // ㄲ
+        .{ '⠠', '⠊' }, // ㄸ
+        .{ '⠠', '⠘' }, // ㅃ
+        .{ '⠠', '⠠' }, // ㅆ
+        .{ '⠠', '⠨' }, // ㅉ
+    };
+    const jung = [_]u21{ '⠣', '⠗', '⠜', 0, '⠎', '⠝', '⠱', '⠌', '⠥', '⠧', 1, '⠽', '⠬', '⠍', '⠏', 2, 3, '⠩', '⠪', '⠺', '⠕' };
+    const jung2 = [_][2]u21{
+        .{ '⠜', '⠗' }, // ㅒ
+        .{ '⠧', '⠗' }, // ㅙ
+        .{ '⠏', '⠗' }, // ㅞ
+        .{ '⠍', '⠗' }, // ㅟ
+    };
+    const jong = [_]u21{ '_', '⠁', 0, 1, '⠒', 2, 3, '⠔', '⠂', 4, 5, 6, 7, 8, 9, 10, '⠢', '⠃', 11, '⠄', '⠌', '⠶', '⠅', '⠆', '⠖', '⠦', '⠲', '⠴' };
+    const jong2 = [_][2]u21{
+        .{ '⠁', '⠁' }, // ㄲ
+        .{ '⠁', '⠄' }, // ㄳ
+        .{ '⠒', '⠅' }, // ㄵ
+        .{ '⠒', '⠴' }, // ㄶ
+        .{ '⠂', '⠁' }, // ㄺ
+        .{ '⠂', '⠢' }, // ㄻ
+        .{ '⠂', '⠃' }, // ㄼ
+        .{ '⠂', '⠄' }, // ㄽ
+        .{ '⠂', '⠦' }, // ㄾ
+        .{ '⠂', '⠲' }, // ㄿ
+        .{ '⠂', '⠴' }, // ㅀ
+        .{ '⠃', '⠄' }, // ㅄ
+    };
+};
+
 pub const KorBrailleCluster = union(enum) {
     single: struct {
         buf: [3]u21,
         len: u8,
     },
     composite: struct {
-        chosung: []const u21,
-        jungsung: []const u21,
-        jongsung: []const u21,
+        buf: [6]u21,
+        chosung: []u21,
+        jungsung: []u21,
+        jongsung: []u21,
     },
     abbrev: struct {
         buf: [2]u21,
         len: u8,
     },
 
-    pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+    pub fn getCodepointLength(self: *const @This()) u8 {
+        switch (self.*) {
+            .single => |*single| {
+                return single.len;
+            },
+            .composite => |*composite| {
+                return @intCast(composite.chosung.len + composite.jungsung.len + composite.jongsung.len);
+            },
+            .abbrev => |*abbrev| {
+                return abbrev.len;
+            },
+        }
+    }
+
+    pub fn asCodepointSlice(self: *const @This()) []const u21 {
+        switch (self.*) {
+            .single => |*single| {
+                return single.buf[0..single.len];
+            },
+            .composite => |*composite| {
+                const len: u8 = @intCast(composite.chosung.len + composite.jungsung.len + composite.jongsung.len);
+                return composite.buf[0..len];
+            },
+            .abbrev => |*abbrev| {
+                return abbrev.buf[0..abbrev.len];
+            },
+        }
+    }
+
+    pub fn format(self: *const @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
         _ = fmt;
         _ = options;
 
-        switch (self) {
-            .single => |single| {
+        switch (self.*) {
+            .single => |*single| {
                 for (0..single.len) |i| {
                     try writer.print("{u}", .{single.buf[i]});
                 }
             },
-            .composite => |composite| {
-                for (composite.chosung) |code_point| {
-                    try writer.print("{u}", .{code_point});
-                }
-                for (composite.jungsung) |code_point| {
-                    try writer.print("{u}", .{code_point});
-                }
-                for (composite.jongsung) |code_point| {
-                    try writer.print("{u}", .{code_point});
+            .composite => |*composite| {
+                const len = self.getCodepointLength();
+                for (0..len) |i| {
+                    try writer.print("{u}", .{composite.buf[i]});
                 }
             },
-            .abbrev => |abbrev| {
+            .abbrev => |*abbrev| {
                 for (0..abbrev.len) |i| {
                     try writer.print("{u}", .{abbrev.buf[i]});
                 }
@@ -83,45 +147,6 @@ pub fn korWordToBraille(slice: []const u8, word_len: *usize) ?KorBrailleCluster 
     }
 }
 
-const kor_char_lookup = struct {
-    const cho = [_]u21{ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
-    const jung = [_]u21{ 'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ' };
-    const jong = [_]u21{ '?', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
-};
-
-const kor_braille_lookup = struct {
-    const cho = [_]u21{ '⠈', 0, '⠉', '⠊', 1, '⠐', '⠑', '⠘', 2, '⠠', 3, '⠛', '⠨', 4, '⠰', '⠋', '⠓', '⠙', '⠚' };
-    const cho2 = [_][2]u21{
-        .{ '⠠', '⠈' }, // ㄲ
-        .{ '⠠', '⠊' }, // ㄸ
-        .{ '⠠', '⠘' }, // ㅃ
-        .{ '⠠', '⠠' }, // ㅆ
-        .{ '⠠', '⠨' }, // ㅉ
-    };
-    const jung = [_]u21{ '⠣', '⠗', '⠜', 0, '⠎', '⠝', '⠱', '⠌', '⠥', '⠧', 1, '⠽', '⠬', '⠍', '⠏', 2, 3, '⠩', '⠪', '⠺', '⠕' };
-    const jung2 = [_][2]u21{
-        .{ '⠜', '⠗' }, // ㅒ
-        .{ '⠧', '⠗' }, // ㅙ
-        .{ '⠏', '⠗' }, // ㅞ
-        .{ '⠍', '⠗' }, // ㅟ
-    };
-    const jong = [_]u21{ '_', '⠁', 0, 1, '⠒', 2, 3, '⠔', '⠂', 4, 5, 6, 7, 8, 9, 10, '⠢', '⠃', 11, '⠄', '⠌', '⠶', '⠅', '⠆', '⠖', '⠦', '⠲', '⠴' };
-    const jong2 = [_][2]u21{
-        .{ '⠁', '⠁' }, // ㄲ
-        .{ '⠁', '⠄' }, // ㄳ
-        .{ '⠒', '⠅' }, // ㄵ
-        .{ '⠒', '⠴' }, // ㄶ
-        .{ '⠂', '⠁' }, // ㄺ
-        .{ '⠂', '⠢' }, // ㄻ
-        .{ '⠂', '⠃' }, // ㄼ
-        .{ '⠂', '⠄' }, // ㄽ
-        .{ '⠂', '⠦' }, // ㄾ
-        .{ '⠂', '⠲' }, // ㄿ
-        .{ '⠂', '⠴' }, // ㅀ
-        .{ '⠃', '⠄' }, // ㅄ
-    };
-};
-
 pub const KorCharIndex = union(enum) {
     chosung: struct {
         i: u8,
@@ -139,21 +164,21 @@ pub const KorCharIndex = union(enum) {
     },
 };
 
-pub fn korCharToIndex(code_point: u21) ?KorCharIndex {
-    const is_single = code_point >= 0x3131 and code_point <= 0x3163;
-    const is_composite = code_point >= 0xAC00 and code_point <= 0xD79D;
-    if (is_single) {
-        if (std.mem.indexOf(u21, &kor_char_lookup.cho, &.{code_point})) |i| {
+pub fn korCharToIndex(codepoint: u21) ?KorCharIndex {
+    const is_single_jamo = codepoint >= 0x3131 and codepoint <= 0x3163;
+    const is_composite = codepoint >= 0xAC00 and codepoint <= 0xD79D;
+    if (is_single_jamo) {
+        if (std.mem.indexOf(u21, &kor_char_lookup.cho, &.{codepoint})) |i| {
             return .{ .chosung = .{ .i = @intCast(i) } };
         }
-        if (std.mem.indexOf(u21, &kor_char_lookup.jung, &.{code_point})) |i| {
+        if (std.mem.indexOf(u21, &kor_char_lookup.jung, &.{codepoint})) |i| {
             return .{ .jungsung = .{ .i = @intCast(i) } };
         }
-        if (std.mem.indexOf(u21, &kor_char_lookup.jong, &.{code_point})) |i| {
+        if (std.mem.indexOf(u21, &kor_char_lookup.jong, &.{codepoint})) |i| {
             return .{ .jongsung = .{ .i = @intCast(i) } };
         }
     } else if (is_composite) {
-        const base = code_point - 0xAC00;
+        const base = codepoint - 0xAC00;
         const cho: u8 = @intCast(base / 28 / 21);
         const jung: u8 = @intCast(base / 28 % 21);
         const jong: u8 = @intCast(base % 28);
@@ -199,8 +224,8 @@ pub fn jongsungToBraille(index: u8) []const u21 {
     }
 }
 
-pub fn korCharToBraille(code_point: u21) ?KorBrailleCluster {
-    if (korCharToIndex(code_point)) |char_index| {
+pub fn korCharToBraille(codepoint: u21) ?KorBrailleCluster {
+    if (korCharToIndex(codepoint)) |char_index| {
         var braille: KorBrailleCluster = undefined;
         switch (char_index) {
             .chosung => |chosung| {
@@ -228,20 +253,33 @@ pub fn korCharToBraille(code_point: u21) ?KorBrailleCluster {
                 @memcpy(braille.single.buf[1 .. slice.len + 1], slice);
             },
             .composite => |composite| {
-                braille = .{
-                    .composite = .{
-                        .chosung = blk: {
-                            if (composite.chosung_i == 11) {
-                                // empty if chosung is 'ㅇ'
-                                break :blk &.{};
-                            } else {
-                                break :blk chosungToBraille(composite.chosung_i);
-                            }
-                        },
-                        .jungsung = jungsungToBraille(composite.jungsung_i),
-                        .jongsung = jongsungToBraille(composite.jongsung_i),
-                    },
-                };
+                braille = .{ .composite = .{
+                    .buf = .{0} ** 6,
+                    .chosung = &.{},
+                    .jungsung = &.{},
+                    .jongsung = &.{},
+                } };
+                var i: u8 = 0;
+
+                // chosung
+                if (composite.chosung_i != 11) {
+                    // empty if chosung is 'ㅇ'
+                    const cho = chosungToBraille(composite.chosung_i);
+                    braille.composite.chosung = braille.composite.buf[i .. i + cho.len];
+                    @memcpy(braille.composite.chosung, cho);
+                    i += @intCast(cho.len);
+                }
+
+                // jungsung
+                const jung = jungsungToBraille(composite.jungsung_i);
+                braille.composite.jungsung = braille.composite.buf[i .. i + jung.len];
+                @memcpy(braille.composite.jungsung, jung);
+                i += @intCast(jung.len);
+
+                // jongsung
+                const jong = jongsungToBraille(composite.jongsung_i);
+                braille.composite.jongsung = braille.composite.buf[i .. i + jong.len];
+                @memcpy(braille.composite.jongsung, jong);
             },
         }
         return braille;
@@ -250,17 +288,47 @@ pub fn korCharToBraille(code_point: u21) ?KorBrailleCluster {
     }
 }
 
+pub fn arrayListAppendKorAsBraille(array_list: *std.ArrayList(u21), input: []const u8) !void {
+    var i: usize = 0;
+    var is_prev_kor = false;
+    var iter_utf8 = (try std.unicode.Utf8View.init(input)).iterator();
+
+    while (iter_utf8.nextCodepointSlice()) |codepoint_slice| {
+        var offset = codepoint_slice.len;
+        defer i += offset;
+
+        const codepoint = try std.unicode.utf8Decode(codepoint_slice);
+        defer is_prev_kor = (codepoint >= 0x3131 and codepoint <= 0x3163) or (codepoint >= 0xAC00 and codepoint <= 0xD79D);
+
+        if (!is_prev_kor) {
+            if (korWordToBraille(input[i..], &offset)) |braille| {
+                try array_list.appendSlice(braille.asCodepointSlice());
+                for (0..offset / 3 - 1) |_| {
+                    _ = iter_utf8.nextCodepointSlice();
+                }
+                continue;
+            }
+        }
+
+        if (korCharToBraille(codepoint)) |braille| {
+            try array_list.appendSlice(braille.asCodepointSlice());
+        } else {
+            try array_list.appendSlice(codepoint);
+        }
+    }
+}
+
 pub fn printKorAsBraille(writer: anytype, input: []const u8) !void {
     var i: usize = 0;
     var is_prev_kor = false;
     var iter_utf8 = (try std.unicode.Utf8View.init(input)).iterator();
 
-    while (iter_utf8.nextCodepointSlice()) |code_point_slice| {
-        var offset = code_point_slice.len;
+    while (iter_utf8.nextCodepointSlice()) |codepoint_slice| {
+        var offset = codepoint_slice.len;
         defer i += offset;
 
-        const code_point = try std.unicode.utf8Decode(code_point_slice);
-        defer is_prev_kor = (code_point >= 0x3131 and code_point <= 0x3163) or (code_point >= 0xAC00 and code_point <= 0xD79D);
+        const codepoint = try std.unicode.utf8Decode(codepoint_slice);
+        defer is_prev_kor = (codepoint >= 0x3131 and codepoint <= 0x3163) or (codepoint >= 0xAC00 and codepoint <= 0xD79D);
 
         if (!is_prev_kor) {
             if (korWordToBraille(input[i..], &offset)) |braille| {
@@ -272,10 +340,10 @@ pub fn printKorAsBraille(writer: anytype, input: []const u8) !void {
             }
         }
 
-        if (korCharToBraille(code_point)) |braille| {
+        if (korCharToBraille(codepoint)) |braille| {
             try writer.print("{s}", .{braille});
         } else {
-            try writer.print("{u}", .{code_point});
+            try writer.print("{u}", .{codepoint});
         }
     }
 }
--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,59 @@ pub fn main() !void {
     var input_buf: [1000]u8 = undefined;
     const input = try cli.stdinReadLine(&input_buf);
 
-    const stdout_wrtier = std.io.getStdOut().writer();
-    try braille_conv.printKorAsBraille(stdout_wrtier, input);
+    const stdout_writer = std.io.getStdOut().writer();
+    try braille_conv.printKorAsBraille(stdout_writer, input);
+}
+
+test "single jamo" {
+    const expectEqualDeep = std.testing.expectEqualDeep;
+
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄱ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠈' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄲ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠠', '⠈' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄴ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠉' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄷ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠊' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄹ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠐' });
+
+    try expectEqualDeep(braille_conv.korCharToBraille('ㅏ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠣' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㅐ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠗' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㅒ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠜', '⠗' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㅚ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠽' });
+
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄳ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠁', '⠄' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄵ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠒', '⠅' });
+    try expectEqualDeep(braille_conv.korCharToBraille('ㄺ').?.asCodepointSlice(), &[_]u21{ '⠿', '⠂', '⠁' });
+}
+
+test "composite char" {
+    const expectEqualDeep = std.testing.expectEqualDeep;
+
+    try expectEqualDeep(braille_conv.korCharToBraille('안').?.asCodepointSlice(), &[_]u21{ '⠣', '⠒' });
+    try expectEqualDeep(braille_conv.korCharToBraille('녕').?.asCodepointSlice(), &[_]u21{ '⠉', '⠱', '⠶' });
+    try expectEqualDeep(braille_conv.korCharToBraille('낮').?.asCodepointSlice(), &[_]u21{ '⠉', '⠣', '⠅' });
+    try expectEqualDeep(braille_conv.korCharToBraille('밤').?.asCodepointSlice(), &[_]u21{ '⠘', '⠣', '⠢' });
+    try expectEqualDeep(braille_conv.korCharToBraille('았').?.asCodepointSlice(), &[_]u21{ '⠣', '⠌' });
+    try expectEqualDeep(braille_conv.korCharToBraille('너').?.asCodepointSlice(), &[_]u21{ '⠉', '⠎' });
+    try expectEqualDeep(braille_conv.korCharToBraille('앉').?.asCodepointSlice(), &[_]u21{ '⠣', '⠒', '⠅' });
+    try expectEqualDeep(braille_conv.korCharToBraille('닭').?.asCodepointSlice(), &[_]u21{ '⠊', '⠣', '⠂', '⠁' });
+}
+
+test "char abbrev" {
+    // TODO
+}
+
+test "word abbrev" {
+    const expectEqualDeep = std.testing.expectEqualDeep;
+
+    var word_len: usize = 0;
+    try expectEqualDeep(braille_conv.korWordToBraille("그래서", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠎' });
+    try expectEqualDeep(braille_conv.korWordToBraille("그러나", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠉' });
+    try expectEqualDeep(braille_conv.korWordToBraille("그러면", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠒' });
+    try expectEqualDeep(braille_conv.korWordToBraille("그러므로", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠢' });
+    try expectEqualDeep(braille_conv.korWordToBraille("그런데", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠝' });
+    try expectEqualDeep(braille_conv.korWordToBraille("그리고", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠥' });
+    try expectEqualDeep(braille_conv.korWordToBraille("그리하여", &word_len).?.asCodepointSlice(), &[_]u21{ '⠁', '⠱' });
+}
+
+test "sentence" {
+    // TODO
 }
